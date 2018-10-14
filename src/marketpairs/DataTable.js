7650887fd97401a13aec16fdddce1dbfaa23d0f8
Akshay Raje <akshay.raje@gmail.com>
2018-10-14 17:52:36 +0530

    row component in datatable

--- src/marketpairs/DataTable.js
+++ src/marketpairs/DataTable.js
@@ -2,6 +2,40 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import BigNumber from 'bignumber.js'
 
+const Row = (props) => (
+    <React.Fragment key={props.symbol}>
+        <div className="d-none d-sm-inline">
+            <Link className="row table-row small py-1" to={`trade/${props.symbol}`}>
+                <div className="col">{props.symbol}</div>
+                <div className="col">{new BigNumber(props.lastPrice).toFormat(null,1)}</div>
+                <div className={props.priceChangePercent < 0 ? 'col text-danger' : 'col text-success'}>{`${new BigNumber(props.priceChangePercent).toFormat(2,1)}%`}</div>
+                <div className="col">{new BigNumber(props.highPrice).toFormat(null,1)}</div>
+                <div className="col">{new BigNumber(props.lowPrice).toFormat(null,1)}</div>
+                <div className="col">{new BigNumber(props.quoteVolume).toFormat(null,1)}</div>
+            </Link>
+        </div>
+        <div className="d-inline d-sm-none">
+            <Link className="row table-row small py-1" to={`trade/${props.symbol}`}>
+                <div className="col-12">
+                    <span className="font-weight-bold">{props.symbol}</span> <span>{new BigNumber(props.lastPrice).toFormat(null,1)}</span> <span className={props.priceChangePercent < 0 ? 'text-danger' : 'text-success'}>{`${new BigNumber(props.priceChangePercent).toFormat(2,1)}%`}</span>
+                </div>
+                <div className="col-4">
+                    <div className="font-weight-light text-muted small">24h High</div> 
+                    <span className="small">{new BigNumber(props.highPrice).toFormat(null,1)}</span>
+                </div>
+                <div className="col-4">
+                    <div className="font-weight-light text-muted small">24h Low</div> 
+                    <span className="small">{new BigNumber(props.lowPrice).toFormat(null,1)}</span>
+                </div>
+                <div className="col-4">
+                    <div className="font-weight-light text-muted small">24h Volume</div> 
+                    <span className="small">{new BigNumber(props.quoteVolume).toFormat(null,1)}</span>
+                </div>
+            </Link>
+        </div>
+    </React.Fragment>    
+)
+
 const DataTable = (props) => {
 
     let rows = [];
@@ -9,40 +43,10 @@ const DataTable = (props) => {
     let numRows = tickerArray.length;
 
     for (var i = 0; i < numRows; i++) {
-        let symbol = tickerArray[i].symbol;
-        if( props.filter.includes(symbol) ){
+        let data = tickerArray[i];
+        if( props.filter.includes(tickerArray[i].symbol) ){
             rows.push(
-                <React.Fragment key={symbol}>
-                    <div className="d-none d-sm-inline">
-                        <Link className="row table-row small py-1" to={`trade/${symbol}`}>
-                            <div className="col">{symbol}</div>
-                            <div className="col">{new BigNumber(tickerArray[i].lastPrice).toFormat(null,1)}</div>
-                            <div className={tickerArray[i].priceChangePercent < 0 ? 'col text-danger' : 'col text-success'}>{`${new BigNumber(tickerArray[i].priceChangePercent).toFormat(2,1)}%`}</div>
-                            <div className="col">{new BigNumber(tickerArray[i].highPrice).toFormat(null,1)}</div>
-                            <div className="col">{new BigNumber(tickerArray[i].lowPrice).toFormat(null,1)}</div>
-                            <div className="col">{new BigNumber(tickerArray[i].quoteVolume).toFormat(null,1)}</div>
-                        </Link>
-                    </div>
-                    <div className="d-inline d-sm-none">
-                        <Link className="row table-row small py-1" to={`trade/${symbol}`}>
-                            <div className="col-12">
-                                <span className="font-weight-bold">{symbol}</span> <span>{new BigNumber(tickerArray[i].lastPrice).toFormat(null,1)}</span> <span className={tickerArray[i].priceChangePercent < 0 ? 'text-danger' : 'text-success'}>{`${new BigNumber(tickerArray[i].priceChangePercent).toFormat(2,1)}%`}</span>
-                            </div>
-                            <div className="col-4">
-                                <div className="font-weight-light text-muted small">24h High</div> 
-                                <span className="small">{new BigNumber(tickerArray[i].highPrice).toFormat(null,1)}</span>
-                            </div>
-                            <div className="col-4">
-                                <div className="font-weight-light text-muted small">24h Low</div> 
-                                <span className="small">{new BigNumber(tickerArray[i].lowPrice).toFormat(null,1)}</span>
-                            </div>
-                            <div className="col-4">
-                                <div className="font-weight-light text-muted small">24h Volume</div> 
-                                <span className="small">{new BigNumber(tickerArray[i].quoteVolume).toFormat(null,1)}</span>
-                            </div>
-                        </Link>
-                    </div>
-                </React.Fragment>
+                <Row {...data} />
             )
         }
     }
